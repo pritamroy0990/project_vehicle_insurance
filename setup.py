edfcb836eed5c97607029b918e7783a97f9a299f
pritamroy0990 <pritamroy0990@gmail.com>
2025-02-13 19:55:09 +0600

    project structure,venv ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="pritam",
+    author_email="pritamroy0990@gmail.com",
+    packages=find_packages()
+)
